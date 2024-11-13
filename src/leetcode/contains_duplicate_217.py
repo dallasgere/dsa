a2d665499476a41607c9215f45c6975b4c2707de
dallas <dallasmgere@gmail.com>
2024-11-13 17:40:53 -0600

    adding in docstrings

---
+++ src/leetcode/contains_duplicate_217.py
@@ -0,0 +1,23 @@
+""" 
+leetcode problem 217
+"""
+
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        """this determines if a list has duplicates or not
+
+        Args:
+            nums (List[int]): the list of numbers to look through
+
+        Returns:
+            bool: true if nums contains duplicate, false otherwise
+        """
+        seen = set()
+        for val in nums:
+            if val not in seen:
+                seen.add(val)
+            else:
+                return True
+
+        return False
