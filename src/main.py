ae7bfcdf67c661e8f9506afec12da273f24945c2
dallas <dallasmgere@gmail.com>
2024-11-14 12:18:08 -0600

    adding another little python example

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# access characters
+string = "this"
+print(string[-1])
 
-for i in range(len(nums) - 1, -1, -1):
-    print(f"i: {i}")
-    print(f"val: {nums[i]}")
+# different looping ranges
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for i in range(len(nums) - 1, -1, -1):
+#     print(f"i: {i}")
+#     print(f"val: {nums[i]}")
