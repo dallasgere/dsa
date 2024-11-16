a974580ecee4594d8698ff861a1e54d0782f4dba
dallas <dallasmgere@gmail.com>
2024-11-15 22:39:57 -0600

    adding valud parenthesis

---
+++ src/leetcode/valid_parenthesis_20.py
@@ -0,0 +1,15 @@
+class ValidParenthesis:
+    def isValid(self, s: str) -> bool:
+        close_open = {")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for c in s:
+            if c in close_open:
+                if len(stack) > 0 and stack[-1] == close_open[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return len(stack) == 0
